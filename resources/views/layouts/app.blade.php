1096b1e3922cd476302165c9a77c3fe1d6ff4ebc
Dotunj <dotunjolaosho@gmail.com>
2017-05-31 00:16:52 +0100

    initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<head>
+   <title>Laravel Quickstart - Intermediate</title>
+
+</head>
+
+<body> 
+	<div class"container">
+		<nav class = "navbar navbar-default">
+		</nav>
+	</div>
+
+	@yield('content')
+
+</body>
+</html>
